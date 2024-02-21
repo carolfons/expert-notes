0a596a514f04a535b09f176cd8a98e4abc71cc8a
CarolFons <carolsantosfonseca@outlook.com>
2024-02-21 19:33:08 -0300

    creating events in the cards

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,31 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import {toast} from 'sonner'
 
 export function NewNoteCard() {
+
+    const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+    const[content, setContent] = useState('')
+
+    function handleStartEditor() {
+        setShouldShowOnboarding(false)
+    }
+
+    function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+        setContent(event.target.value)
+        if (event.target.value == '') {
+            setShouldShowOnboarding(true)
+        }
+    }
+
+    function handleSaveNote(event: FormEvent) {
+        event.preventDefault()
+        console.log(content)
+        toast.success('Nota criada com sucesso')
+
+    }
+
     return (
         <Dialog.Root>
             <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 text-left gap-3 outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -12,21 +36,32 @@ export function NewNoteCard() {
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50' />
                 <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+
                     <Dialog.DialogClose className=' absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='w-5 h-5' />
                     </Dialog.DialogClose>
-                    <div className='flex flex-1 flex-col gap-3 p-5'>
-                        <span className="text-sm font-medium text-slate-300">
-                           Adicionar Nota
-                        </span>
-                        <p className=' text-sm leading-6 text-slate-400'>
-                        Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
-                        </p>
-                    </div>
-                    <button type='button'
-                        className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'>
-                        Salvar Nota
-                    </button>
+
+                    <form onSubmit={handleSaveNote} className='flex-1 flex-col flex'>
+                        <div className='flex flex-1 flex-col gap-3 p-5'>
+                            <span className="text-sm font-medium text-slate-300">
+                                Adicionar Nota
+                            </span>
+                            {/* se 'shouldShowOnboarding' for true, colocar texto original, se não mostrar a <textarea> */}
+                            {shouldShowOnboarding ? (
+                                <p className=' text-sm leading-6 text-slate-400'>
+                                    Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
+                                </p>
+                            ) : (
+                                <textarea autoFocus
+                                    className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                                    onChange={handleContentChanged} />
+                            )}
+                        </div>
+                        <button type='button'
+                            className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500'>
+                            Salvar Nota
+                        </button>
+                    </form>
                 </Dialog.Content>
             </Dialog.Portal>
         </Dialog.Root>
